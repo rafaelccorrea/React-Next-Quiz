993fad818f576f209166d50d10c29be053daa6c8
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-03-21 17:33:50 -0300

    Buscando ID da pergunta na API

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -2,5 +2,14 @@ import questoes from "../database";
 
 /* eslint-disable import/no-anonymous-default-export */
 export default (req, res) => {
+  const id = +req.query.id;
+  const questao = questoes.filter(questao => questao.id === id)
+
+  if(questao.length === 1){
+    const questaoSelect = questao[0]
+    res.status(200).json(questaoSelect.toObject())
+  }else{
+    res.status(404).send()
+  }
   res.status(200).json(questoes[0].toObject());
 };
