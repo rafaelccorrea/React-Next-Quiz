668a87b80efdbf403e4dbeddcd121576afd50bc8
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-03-21 22:14:44 -0300

    Criação de cores

--- components/questao/Questao.tsx
+++ components/questao/Questao.tsx
@@ -1,27 +1,36 @@
 import Enunciado from "../enuciado/Enunciado";
 import Resposta from "../resposta/Resposta";
 import QuestaoProps from "./model";
-import styles from './Questao.module.css';
+import styles from "./Questao.module.css";
 
-export default function Questao(props:QuestaoProps) {
-    const questao = props.valor
+const letras = [
+  { valor: "A", cor: "#F2C866" },
+  { valor: "B", cor: "#F266BA" },
+  { valor: "C", cor: "#85D4F2" },
+  { valor: "D", cor: "#BCE596" },
+];
 
-    function renderRespostas() {
-        return questao.resposta.map((resposta, i) => {
-            return <Resposta 
-                key={i}
-                valor={resposta}
-                indice={i}
-                letra="A"
-                corFundoLetra="#F2C866"
-            />
-        })
-    }
+export default function Questao(props: QuestaoProps) {
+  const questao = props.valor;
 
-    return (
-        <div className={styles.questao}>
-            <Enunciado texto={questao.enunciado} />
-            {renderRespostas()}
-        </div>
-    )
+  function renderRespostas() {
+    return questao.resposta.map((resposta, i) => {
+      return (
+        <Resposta
+          key={i}
+          valor={resposta}
+          indice={i}
+          letra={letras[i].valor}
+          corFundoLetra={letras[i].cor}
+        />
+      );
+    });
+  }
+
+  return (
+    <div className={styles.questao}>
+      <Enunciado texto={questao.enunciado} />
+      {renderRespostas()}
+    </div>
+  );
 }
