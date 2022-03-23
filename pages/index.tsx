9abaee0a8dee380529a66dd3f6390020e7fc602e
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-03-22 21:02:47 -0300

    Criando Estado para questionario e integrando API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,50 @@
-import { useState } from 'react'
-import Questionario from '../components/questionario/Questionario'
-import QuestaoModel from '../model/questao'
-import RespostaModel from '../model/resposta'
+import { useEffect, useState } from "react";
+import Questionario from "../components/questionario/Questionario";
+import QuestaoModel from "../model/questao";
+import RespostaModel from "../model/resposta";
 
-const questaoTeste = new QuestaoModel(1, 'Melhor Cor?', [
-  RespostaModel.errada('Verde'),
-  RespostaModel.errada('Amarela'),
-  RespostaModel.errada('Azul'),
-  RespostaModel.certa('Preta'),
-  
-])
+const questaoTeste = new QuestaoModel(1, "Melhor Cor?", [
+  RespostaModel.errada("Verde"),
+  RespostaModel.errada("Amarela"),
+  RespostaModel.errada("Azul"),
+  RespostaModel.certa("Preta"),
+]);
+
+const BASE_URL = "http://localhost:3000/api"
 
 export default function Home() {
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoTeste);
+  const [idsQuest, setIdsQuest] = useState<number[]>([]);
 
-  const [ questao, setQuestao ] = useState(questaoTeste)
-
-  function questaoRespondida (questao: QuestaoModel) {
-
+  async function carregarGetIds() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsQuest = await resp.json();
+    setIdsQuest(idsQuest);
   }
 
-  function proximo () {
-    
+  async function carregarIdsQuestao( idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json();
   }
+
+  useEffect(() => {
+    carregarGetIds()
+  }, [])
+
+  useEffect(() => {
+    idsQuest.length > 0 && carregarIdsQuestao(idsQuest[0])
+  }, [idsQuest])
+
+  function questaoRespondida(questao: QuestaoModel) {}
+
+  function proximo() {}
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-      <Questionario 
-        questao={questao}
-        ultima={true}
-        questaoRespondida={questaoRespondida}
-        proximo={proximo}
-      />
-    </div>
-  )
+    <Questionario
+      questao={questao}
+      ultima={true}
+      questaoRespondida={questaoRespondida}
+      proximo={proximo}
+    />
+  );
 }
